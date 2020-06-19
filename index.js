6be346112b6882b2072c6ed26aed46855eeb84ca
Isaac Householder <isaachouseholder@gmail.com>
2020-06-19 15:51:07 -0700

    port update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -14,6 +16,6 @@ io.on('connection', (socket) => {
 });
 
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(PORT || 3000, () => {
+  console.log(`listening on port ${PORT}`);
 });
